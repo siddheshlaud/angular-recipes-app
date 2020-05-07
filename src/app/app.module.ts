81314e5e7d3d0a925c7147b2723afbfaf45bdd46
siddheshlaud <sidd.laud@gmail.com>
2020-05-07 18:29:36 +0530

    Shopping List Template Driven Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { DropdownDirective } from './directives/dropdown.directive';
 import { AppRoutesModule } from './app-routes/app-routes.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   ],
   imports: [
     BrowserModule,
-    AppRoutesModule
+    AppRoutesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
