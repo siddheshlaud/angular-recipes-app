ad2b1bb64ef27c9abdc186bfc554c2c626fdb9d1
siddheshlaud <sidd.laud@gmail.com>
2020-05-14 14:13:37 +0530

    New way for Lazy Loading

--- src/app/app-routes/app-routes.module.ts
+++ src/app/app-routes/app-routes.module.ts
@@ -7,7 +7,11 @@ import { AuthGuard } from "../auth/auth.guard";
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   { path: "auth", component: AuthComponent },
-  { path: "recipes", loadChildren: '../recipes/recipes.module#RecipesModule' }
+  {
+    path: "recipes",
+    loadChildren: () =>
+      import("../recipes/recipes.module").then(m => m.RecipesModule)
+  }
 ];
 
 @NgModule({
